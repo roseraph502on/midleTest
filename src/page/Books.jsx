048d222c1327c21660b7e50620edcb9b301dc2ec
roseraph502on <sera6768@naver.com>
2025-04-17 22:03:28 +0900

    3

--- src/page/Books.jsx
+++ src/page/Books.jsx
@@ -3,27 +3,9 @@ import { useState ,useEffect} from 'react'
 import BookCard from '../component/BookCard'
 
 const Books = () => {
-//   const [bookList, setBookList] = useState([]);
-//   const[query, setqeury] = useSearchParams();
-//     const getBooks = async () => {
-//         // let searchQuery = query.get("q")||("");
-//         console.log("쿼리값",searchQuery)
-//         let url = `https://openlibrary.org/search.json`
-//         let response = await fetch(url);
-//         let data = await response.json();
-//         setproductList(data);
-//       }
-
-//       useEffect(() => {
-//         getBooks();
-//       }, [query])
 
   return (
     <div id='books'>
-                  {/* (bookList.map((menu) => (
-            <BookCard item={menu} />
-
-          ))) */}
           <BookCard/>
     </div>
   )
--- src/stores/store.jsx
+++ src/stores/store.jsx
@@ -1,12 +1,7 @@
 import { create } from "zustand";
 const store = create((set)=>({
-    user: null, // 로그인한 사용자 정보 (없으면 null)
-    isLoggedIn: false, // 로그인 상태
+    isLoggedIn: false,
   
-    // 로그인 함수 (예: 서버 응답 후 user 정보 저장)
     login: (userData) => set({ user: userData, isLoggedIn: true }),
-  
-    // 로그아웃 함수
-    logout: () => set({ user: null, isLoggedIn: false }),
 }));
 export default store
